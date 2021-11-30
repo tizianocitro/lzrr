c13513f02a964325e084ac7d8c15a7bc8f9b58e5
Tiziano <tizianocitro98@gmail.com>
2021-11-30 18:20:20 +0100

    Tables implemented

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 
 from module.compress import compress_all
 from module.stats import get_all_stats
-from module.plots import from_stats_to_bar, plot_all
-import matplotlib.pyplot as plt
+from module.plots import plot_all
+from module.tables import create_all_tables
+
 cwd = os.getcwd()
-#compress_all(cwd)
+compress_all(cwd)
 stats = get_all_stats(cwd)
 plot_all(stats)
-
+create_all_tables(stats)
